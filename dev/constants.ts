67d64231f3f5e886817e265e8658e33024bc6c55
Huy-DNA <huydo862003@gmail.com>
2026-07-29 10:02:48 +0700

    refactor: remove everything

--- dev/constants.ts
+++
@@ -1,2 +0,0 @@
-export const EDITOR_HOST = 'localhost';
-export const EDITOR_PORT = 4001;
--- dev/vite-plugin/client/services/http.client.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import {
-  EDITOR_HOST, EDITOR_PORT,
-} from '@dev/constants';
-
-export const httpClient = axios.create({
-  baseURL: `http://${EDITOR_HOST}:${EDITOR_PORT}/dev`,
-});
